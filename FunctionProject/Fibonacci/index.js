d4256593facace261180351dde3359d8f9e0aaff
Fataltester229 <juanm46891@gmail.com>
2025-11-23 20:11:19 -0500

    implementacion memo fibonacci

--- FunctionProject/Fibonacci/index.js
+++ FunctionProject/Fibonacci/index.js
@@ -1,27 +1,44 @@
 var bigInt = require("big-integer");
+
 module.exports = async function (context, req) {
-    context.log('JavaScript HTTP trigger function processed a request.');
+    context.log('JavaScript HTTP Fibonacci recursivo con memorizacion.');
 
-    let nth = req.body.nth
-    let nth_1 = bigInt.one;
-    let nth_2 = bigInt.zero;
-    let answer = bigInt.zero;
+    let n = req.body?.n ?? req.query?.n;
+    let memo = {
+        0: bigInt.zero,
+        1: bigInt.one
+    };
 
-    if (nth < 0)
-        throw 'must be greater than 0'
-    else if (nth === 0)
-        answer = nth_2
-    else if (nth === 1)
-        answer = nth_1
-    else {
-        for (var i = 0; i < nth - 1; i++) {
-            answer = nth_2.add(nth_1)
-            nth_2 = nth_1
-            nth_1 = answer
-        }
+    if (n === undefined || n === null) {
+        context.res = {
+            status: 400,
+            body: "Debe enviar un número en req.body.n o ?n="
+        };
+        return;
     }
 
+    n = parseInt(n);
+
+    if (n < 0) {
+        context.res = {
+            status: 400,
+            body: "El número debe ser mayor o igual a 0"
+        };
+        return;
+    }
+
+    function fibonacci(k) {
+        if (memo[k] !== undefined) {
+            return memo[k];
+        }
+        memo[k] = fibonacci(k - 1).add(fibonacci(k - 2));
+        return memo[k];
+    }
+
+    let result = fibonacci(n);
+
     context.res = {
-        body: answer.toString()
+        status: 200,
+        body: result.toString()
     };
-}
+};
